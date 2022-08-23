d9940d342bb3f1d685f0c45ab90520796096a354
benitodev <benideveloperr@gmail.com>
2022-08-23 17:34:00 -0500

    service -videos

--- src/services/video.ts
+++ src/services/video.ts
@@ -2,5 +2,6 @@ import { Video } from '../types'
 
 export const getVideos = async (): Promise<Video[]> => {
   const response = await fetch('https://react-entertainment-default-rtdb.europe-west1.firebasedatabase.app/entries.json')
+
   return response.json()
 }
