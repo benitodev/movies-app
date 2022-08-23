2847f3655e59f70c7e2023a0bcb1933c2794f586
benitodev <benideveloperr@gmail.com>
2022-08-23 17:31:29 -0500

    finished

--- src/reducer/reducer.ts
+++ src/reducer/reducer.ts
@@ -2,6 +2,7 @@ import { videoReducer } from '../slices/video'
 import { searchReducer } from '../slices/search'
 import { filterReducer } from '../slices/filter'
 import { savedReducer } from '../slices/saved'
+
 const reducer = {
   videoReducer,
   searchReducer,
